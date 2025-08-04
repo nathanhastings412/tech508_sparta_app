4489cef3c053db874f6ad25e7a588c89a98bdf41
Nathan Hastings <nathanhastings412@gmail.com>
2025-08-04 14:18:27 +0100

    updated scripts

--- notes/launching_app/prov-app.sh
+++ notes/launching_app/prov-app.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 # provision sparta test app
-
-echo "update..."
-sudo apt-get update
-echo "done"
-echo
-
-
-# FIX
-echo "upgrade..."
-sudo apt upgtrade -y
+echo =============================
+echo "update and upgrade packages"
+echo =============================
+sudo DEBIAN_FRONTEND=noninteractive apt-get update -y
+sudo DEBIAN_FRONTEND=noninteractive apt-get upgrade -yq
 echo "done"
 echo
 
 # FIX
-echo "install niginx"
+echo ===============
+echo "install nginx"
+echo ===============
 sudo apt-get install nginx -y
 echo "done"
 echo
@@ -22,45 +19,59 @@ echo
 # configure reverse proxy using nginx
 
 # installing NodeJS v20 (installs node and npm commands)
-
+echo ====================
 echo "install NodeJS v20"
+echo ====================
 sudo DEBIAN_FRONTEND=noninteractive bash -c "curl -fsSL https://deb.nodesource.com/setup_20.x | bash -" && \
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y nodejs
 echo "done"
 echo 
 
 # verify nodejs version
+echo ==============
 echo "node version"
+echo ==============
 node -v
-echo 
+echo done 
+echo
 
 # download app code
+echo ===================
 echo "download app code"
+echo ===================
 git clone https://github.com/nathanhastings412/tech508_sparta_app repo
 echo "done"
 echo
 
 # cd into app folder
+echo ====================
 echo "cd into app folder"
+echo ====================
 cd repo/app
 echo "done"
 echo
 
 # set env var for connection to database
 # comment this out
+echo ======================================
 echo "set connection for database variable"
+echo ======================================
 export DB_HOST=mongodb://172.31.25.124:27017/posts
 echo "done"
 echo
 
 # install packages for app
+echo ======================
 echo "install app packages"
+echo ======================
 npm install
 echo "done"
 echo
 
 # start app
+echo ===========
 echo "start app"
+echo ===========
 npm start
 echo "done"
 
--- notes/launching_app/prov-db.sh
+++ notes/launching_app/prov-db.sh
@@ -1,28 +1,26 @@
 #!/bin/bash
 
-# update packages
-echo "update..."
-sudo apt-get update
-echo "done"
-echo
-
-
-# upgrade packages
-echo "upgrade..."
-sudo apt upgrade -y
+# update and upgrade packages
+echo =============================
+echo "update and upgrade packages"
+echo =============================
+sudo DEBIAN_FRONTEND=noninteractive apt-get update -y
+sudo DEBIAN_FRONTEND=noninteractive apt-get upgrade -yq
 echo "done"
 echo
 
 # install gnupg and curl
+echo ========================
 echo "install gnupg and curl"
+echo ========================
 sudo apt-get install gnupg curl
 echo "done"
 echo 
 
-
-
 # import mongoDB public GBG key
+echo ================
 echo "import mongodb"
+echo ================
 curl -fsSL https://www.mongodb.org/static/pgp/server-7.0.asc | \
    sudo gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg \
    --dearmor
@@ -30,19 +28,25 @@ echo "done"
 echo
 
 # create the list file
+echo =============
 echo "create list"
+echo =============
 echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
 echo "done"
 echo
 
 # Reload package database
+echo =========================
 echo "reload package database"
+echo =========================
 sudo apt-get update
 echo "done"
 echo
 
 # install mongoDB community server FIX!
+echo ==================================
 echo "install mongoDB community server"
+echo ==================================
 sudo apt-get install -y \
    mongodb-org=7.0.22 \
    mongodb-org-database=7.0.22 \
